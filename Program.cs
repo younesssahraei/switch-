51cd904e49a7b1a519c94aeb84f1d3b6e4050ec3
younesssahraei <127088769+younesssahraei@users.noreply.github.com>
2023-03-11 01:48:45 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// youness sahraei
+
+using System.Diagnostics;
+
+Console.WriteLine("are you married? (y or n)");
+string cnt = Console.ReadLine();
+
+switch (cnt.ToLower())
+{
+    case "y":
+                switch (  "do you have children?(y or n)");
+                case "y":       
+                             switch ("are you satisfied with your children?");
+                                 case "y":
+                                     switch ("god bless them");
+                case "n":
+                     switch ("treat them well");
+                        case "n": 
+              switch ("do you want to have a baby?");
+                 case "y":
+                     switch ("consult your wife");
+                        case "n":
+                           switch("being a father is a good feeling");
+                             break;
+     case "n":
+       switch ("are you planning to get married?");
+               case "y":
+                 switch ("have a good father");
+               case "n":
+                  switch("god willing you will get married");
+                 break;
+        Console.ReadKey();
+
+}
+
+
