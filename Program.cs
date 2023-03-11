0d39bb2a96fcd802b4f1028346a9e8ce073d257d
younesssahraei <127088769+younesssahraei@users.noreply.github.com>
2023-03-11 19:35:00 +0330

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,28 +8,29 @@ string cnt = Console.ReadLine();
 switch (cnt.ToLower())
 {
     case "y":
-                switch (  "do you have children?(y or n)");
-                case "y":       
-                             switch ("are you satisfied with your children?");
-                                 case "y":
-                                     switch ("god bless them");
-                case "n":
-                     switch ("treat them well");
-                        case "n": 
-              switch ("do you want to have a baby?");
-                 case "y":
-                     switch ("consult your wife");
-                        case "n":
-                           switch("being a father is a good feeling");
+
+          { switch (  "do you have children?(y or n)");
+             case "y":       
+                       switch ("are you satisfied with your children?(y or n)");
+                          case "y":
+                            switch ("god bless them");
+                          case "n":
+                        switch ("treat them well");
+                         case "n": 
+                        switch ("do you want to have a baby?(y or n)");
+                          case "y":
+                        switch ("consult your wife");
+                          case "n":
+          }  switch("being a father is a good feeling");
                              break;
      case "n":
-       switch ("are you planning to get married?");
+         { switch ("are you planning to get married?(y or n)");
                case "y":
                  switch ("have a good father");
                case "n":
-                  switch("god willing you will get married");
+         }  switch("god willing you will get married");
                  break;
-        Console.ReadKey();
+              Console.ReadKey();
 
 }
 
